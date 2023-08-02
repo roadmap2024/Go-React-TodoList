b95b50d8c0c994cdd5cdd3f007fefa44d99773fa
beautylotus <beautylotus718@gmail.com>
2023-08-01 23:54:03 -0700

    update Add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography';
+
+// Import components section
+import Add from './components/Add';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ marginRight: "auto", marginLeft: "auto", width: "80%" }}>
+      <Typography variant='h2' sx={{ textAlign: "center", marginTop: "30px" }}>TodoList</Typography>
+      
+      <Add />
+    </Box>
   );
 }
 
